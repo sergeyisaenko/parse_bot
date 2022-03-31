4a67db8d20014de5e8fd6ea94e1518d74eab0d2d
sergiy_isaienko <sergey.isaienko@gmail.com>
2022-03-31 12:06:33 +0300

    added project and main files

---
+++ config.py
@@ -0,0 +1,3 @@
+URL = 'https://epicentrk.ua/ua/shop/krossovki-i-kedy-muzhskie/?PAGEN_1={}'
+TOKEN = '5252080700:AAGYpZP4IrWC0YgNko_m2WWIpPy-j9Y8Gx8'
+DRIVER_PATH = 'D:\PythonProjects\parse\chromedriver'
---
+++ main.py
@@ -0,0 +1,6 @@
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher, executor, utils, types
+from aiogram.types import ParseMode
+
+from config import TOKEN, URL
